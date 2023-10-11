ef31126c8dde24a7aadddf81b56f53ed5f1a0d0a
ㅤlucasㅤ <64121741+lucazzzkk@users.noreply.github.com>
2023-10-11 03:05:33 -0300

    update kernel.sh again

--- kernel.sh
+++ kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TG_TOKEN=$1
-TG_CHAT=$2
+TG_TOKEN=5723561635:AAF0YhtzMDCGyDc5S4JdLThOQsAc3_Mw8l8
+TG_CHAT=-1001950803691
 TAG="$(curl -s https://api.github.com/repos/tiann/KernelSU/releases/latest | jq -r '.tag_name')"
 
 if [ -z "$TG_TOKEN" ] || [ -z "$TG_CHAT" ] || [ -z "$TAG" ]; then
@@ -13,8 +13,8 @@ KZIP="$(pwd)/Kernel"
 cd ~
 mkdir kernel; cd kernel
 git config --global color.ui true
-git config --global user.name Gabriel2392
-git config --global user.email gabriel824m@gmail.com
+git config --global user.name lucazzzkk 
+git config --global user.email m07403441@gmail.com
 
 function tg_sendFile() {
 		curl -s "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
@@ -38,7 +38,7 @@ n='
 tg_sendText "Starting KernelSU CI Builds ($TAG)"
 today=$(date +%y%m%d)
 
-branches=$(curl -s "https://api.github.com/repos/Gabriel2392/android_kernel_samsung_a33x/branches" | jq -r '.[].name')
+branches=$(curl -s "https://api.github.com/repos/lucazzzkk/android_kernel_samsung_s5e8535/branches" | jq -r '.[].name')
 
 HOME="$(pwd)"
 git clone https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r416183b "$HOME/clang-r416183b" --depth=1
@@ -50,7 +50,7 @@ export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export PATH="$HOME/clang-r416183b/bin:$PATH"
 export PATH="$HOME/build-tools/path/linux-x86:$PATH"
-export TARGET_SOC=s5e8825
+export TARGET_SOC=s5e8535
 export LLVM=1 LLVM_IAS=1
 export ARCH=arm64
 EXTRA_FLAGS="LOCALVERSION=-KernelSU-${TAG}"
@@ -58,13 +58,13 @@ EXTRA_FLAGS="LOCALVERSION=-KernelSU-${TAG}"
 
 for branch in $branches; do
     rm -rf src
-    git clone https://github.com/Gabriel2392/android_kernel_samsung_a33x -b $branch --depth=1 src || continue
+    git clone https://github.com/lucazzzkk/android_kernel_samsung_s5e8535 -b $branch --depth=1 src || continue
     cd src
     curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - || continue
     kversion=$(awk -F= '/^VERSION =/ {v=$2} /^PATCHLEVEL =/ {p=$2} /^SUBLEVEL =/ {s=$2} END {gsub(/ /,"",v); gsub(/ /,"",p); gsub(/ /,"",s); print v"."p"."s}' Makefile | sort)
     echo "Building $kversion"
-    make -j$(nproc --all) -C $(pwd) $EXTRA_FLAGS CROSS_COMPILE="$HOME/clang-r416183b/bin/aarch64-linux-gnu-" CC="$HOME/clang-r416183b/bin/clang" TARGET_SOC=s5e8825 LLVM=1 LLVM_IAS=1 ARCH=arm64 PLATFORM_VERSION=13 ANDROID_MAJOR_VERSION=t KBUILD_BUILD_USER=Gabriel KBUILD_BUILD_HOST=KSUCI a33x_defconfig || continue
-    make -j$(nproc --all) -C $(pwd) $EXTRA_FLAGS CROSS_COMPILE="$HOME/clang-r416183b/bin/aarch64-linux-gnu-" CC="$HOME/clang-r416183b/bin/clang" TARGET_SOC=s5e8825 LLVM=1 LLVM_IAS=1 ARCH=arm64 PLATFORM_VERSION=13 ANDROID_MAJOR_VERSION=t KBUILD_BUILD_USER=Gabriel KBUILD_BUILD_HOST=KSUCI || continue
+    make -j$(nproc --all) -C $(pwd) $EXTRA_FLAGS CROSS_COMPILE="$HOME/clang-r416183b/bin/aarch64-linux-gnu-" CC="$HOME/clang-r416183b/bin/clang" TARGET_SOC=s5e8535 LLVM=1 LLVM_IAS=1 ARCH=arm64 PLATFORM_VERSION=13 ANDROID_MAJOR_VERSION=t KBUILD_BUILD_USER=Gabriel KBUILD_BUILD_HOST=KSUCI a14x_defconfig || continue
+    make -j$(nproc --all) -C $(pwd) $EXTRA_FLAGS CROSS_COMPILE="$HOME/clang-r416183b/bin/aarch64-linux-gnu-" CC="$HOME/clang-r416183b/bin/clang" TARGET_SOC=s5e8535 LLVM=1 LLVM_IAS=1 ARCH=arm64 PLATFORM_VERSION=13 ANDROID_MAJOR_VERSION=t KBUILD_BUILD_USER=Gabriel KBUILD_BUILD_HOST=KSUCI || continue
     cp "arch/arm64/boot/Image" "${KZIP}/Image"
     cd "$KZIP"
     zip -r "KernelSU_${TAG}-${kversion}.zip" ./
